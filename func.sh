274609d00f0b8b9b40b06bbd8f21d37ec8e04d8d
v3l0c1r4pt0r <v3l0c1r4pt0r@gmail.com>
2015-11-08 15:33:08 +0100

    Count prefix of target line
    Add function that counts what a line with desired byte will start
    with.

--- func.sh
+++ func.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 # function library for coloring book
+
+hexpat='^0x[0-9a-fA-F]*$'
+
+# convert input number to hexadecimal form
 function tohex {
     if [ $# -ne 1 ]; then
         echo "Error! Invalid arguments for tohex()";
         exit 1;
     fi;
-    hexpat='^0x[0-9a-f]*$'
     decpat='^[0-9]*$'
     if [[ $1 =~ $hexpat ]]; then
         echo $1;
@@ -16,4 +19,20 @@ function tohex {
         echo "Error! Invalid input for tohex()"
         exit 2
     fi;
+}
+
+# get first byte number in line that contains byte given
+function getlinecaption {
+    if [ $# -ne 1 ]; then
+        echo "Error! Invalid arguments for getlinecaption()";
+        exit 1;
+    fi;
+    input=$1
+    if [[ $input =~ $hexpat ]]; then
+        input=${input//0x/};
+    fi;
+    input=${input^^};
+    comm='obase=16;ibase=16;@@@ / 10 * 10'
+    res=$(echo "${comm//@@@/$input}" | bc);
+    printf "%08s\n" ${res,,} | sed 's/\s/0/g';
 }
