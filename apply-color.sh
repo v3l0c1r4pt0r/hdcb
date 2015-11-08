89acd6276c6991aa2688965b75f386132c3df208
v3l0c1r4pt0r <v3l0c1r4pt0r@gmail.com>
2015-11-08 15:01:28 +0100

    Add input checks and coloring stub
    - add sequences to turn coloring on and off
    - check if byte given is hex whether decimal

--- apply-color.sh
+++ apply-color.sh
@@ -1,16 +1,32 @@
 #!/bin/sh
 # apply color to part of hexdump output
-# Usage: hexdump -C file | apply-color color start-byte [length=4]
-# where color is number 0-15, start-byte and length could be decimal or hex
+# Usage: hexdump -C file | apply-color fg-color bg-color start-byte [length=4]
+# where colors are number 1-255, start-byte and length could be decimal or hex
 if [ $# -lt 2 ]; then
     echo "Error! Too few arguments";
     exit 1;
 fi;
-color=$1;
-sbyte=$2;
+fgcolor=$1;
+bgcolor=$2;
+sbyte=$3;
 length=4;
-if [ $# -ge 3 ]; then
-    length=$3;
+bgcmd="\e[48;5;@@@m"
+fgcmd="\e[38;5;@@@m"
+offcmd="\033[0m"
+if [ $# -ge 4 ]; then
+    length=$4;
 fi;
-echo "color $length bytes starting on $sbyte-th byte to color number $color";
+# echo "color $length bytes starting on $sbyte-th byte to color number $bgcolor";
+hexpat='^0x[0-9a-f]*$'
+decpat='^[0-9]*$'
+if [[ $sbyte =~ $hexpat ]]; then
+    echo "hexed sbyte"
+elif [[ $sbyte =~ $decpat ]]; then
+    echo "decimal sbyte"
+else
+    echo "Error! Invalid input"
+    exit 2
+fi;
+clr=${fgcmd//@@@/$fgcolor}${bgcmd//@@@/$bgcolor}
+# echo -e "\e[38;5;1m$clr chuj $offcmd"
 exit 0;
