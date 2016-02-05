24153f30d47483534d22cf95d7be688f0b9cc05d
v3l0c1r4pt0r <v3l0c1r4pt0r@gmail.com>
2016-02-05 18:58:20 +0100

    Coloring function
    Color applying function can now successfully apply color to current
    line

--- color.c
+++ color.c
@@ -2,11 +2,16 @@
 #include <regex.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 #include "color.h"
 
-int isescape(char *buf);
-char *skipcolor(char *buf);
+int isescape(int c);
+int ishex(int c);
+size_t skipcolor(char* buf);
+
+const int OFFSET_SIZE = 8;
+const uint8_t OFFSET_MASK = 0xf0, LENGTH_MASK = 0x0f;
 
 int apply_to_line(char* line, coloring_descr_t* descr)
 {
@@ -14,30 +19,95 @@ int apply_to_line(char* line, coloring_descr_t* descr)
      * 000001c0  [38;5;0m[48;5;3m01 00 [0m[38;5;15m[48;5;4mee [0m[38;5;0m[48;5;5mff ff ff [0m[38;5;0m[48;5;6m01 00  00 00 [0m[38;5;0m[48;5;7m2e 60 38 3a [0m[38;5;0m[48;5;2m00 [0m[38;5;0m[48;5;3m00[0m  |[38;5;0m[48;5;3m..[0m[38;5;15m[48;5;4m.[0m[38;5;0m[48;5;5m...[0m[38;5;0m[48;5;6m....[0m[38;5;0m[48;5;7m.`8:[0m[38;5;0m[48;5;2m.[0m[38;5;0m[48;5;3m.[0m|
      * 000001a0  00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00  |................|
      */
-    char *cursor = line;
+    char *cursor = line + OFFSET_SIZE;
+    int8_t line_off = 0;
+    uint8_t end_off = offset(descr) + length(descr);
+    printf("%.*s", OFFSET_SIZE, line);
     while(*cursor != '\0')
     {
-        if(isescape(cursor))
+        if(isescape(*cursor))
         {
-            cursor = skipcolor(cursor);
+            size_t esc_size = skipcolor(cursor);
+            printf("%.*s", esc_size, cursor);
+            cursor += esc_size;
+            continue;
         }
-        else
+        if(end_off == line_off)
         {
-            printf("%c", *cursor);
-            cursor++;
+            printf("\e[0m");
         }
+        if(line_off == offset(descr))
+        {
+            printf("\e[48;5;%dm\e[38;5;%dm", descr->bg, descr->fg);
+        }
+        if(isspace(*cursor))
+        {
+            printf(" ");
+            ++cursor;
+            continue;
+        }
+        if(ishex(*cursor) && ishex(*(cursor + 1)))
+        {
+            printf("%c%c", *cursor, *(cursor + 1));
+            ++line_off;
+            cursor += 2;
+            continue;
+        }
+        if(line_off == 16)
+        {
+            printf("|");
+            ++cursor;
+            for(line_off = 0; line_off > -16; line_off--)
+            {
+                while(isescape(*cursor))
+                {
+                    size_t esc_size = skipcolor(cursor);
+                    printf("%.*s", esc_size, cursor);
+                    cursor += esc_size;
+                }
+                if(end_off == -line_off)
+                {
+                    printf("\e[0m");
+                }
+                if(-line_off == offset(descr))
+                {
+                    printf("\e[48;5;%dm\e[38;5;%dm", descr->bg, descr->fg);
+                }
+                printf("%c", *cursor);
+                ++cursor;
+            }
+            while(isescape(*cursor))
+            {
+                size_t esc_size = skipcolor(cursor);
+                printf("%.*s", esc_size, cursor);
+                cursor += esc_size;
+            }
+            printf("|\n");
+            return 1;
+        }
+        ++cursor;
     }
+    return 0;
 }
 
-inline int isescape(char* buf)
+inline int isescape(int c)
 {
-    if(*buf == '\e')
+    if(c == '\e')
         return 1;
     else
         return 0;
 }
 
-char* skipcolor(char* buf)
+int ishex(int c)
+{
+    if((c >= '0' && c <= '9') || (c >= 'a' && c <= 'f') ||
+        (c >= 'A' && c <= 'F'))
+        return 1;
+    else
+        return 0;
+}
+
+size_t skipcolor(char* buf)
 {
     regex_t regex;
     regmatch_t match[2];
@@ -52,19 +122,27 @@ char* skipcolor(char* buf)
     {
         fprintf(stderr, "regcomp error %d\n",error);
         exit(1);
-        return buf;
     }
     if(error = regexec(&regex, buf, 2, match, 0))
     {
         fprintf(stderr, "regexec error %d\n",error);
         regfree(&regex);
-        exit(1);
-        return buf;
+        return 1;
     }
     if(match[1].rm_so != -1)
     {
-        buf += match[1].rm_so;
+        return match[1].rm_so;
     }
     regfree(&regex);
-    return buf;
+    return 1;
+}
+
+inline uint8_t length(coloring_descr_t* descr)
+{
+    return descr->offlen & LENGTH_MASK;
+}
+
+inline uint8_t offset(coloring_descr_t* descr)
+{
+    return descr->offlen >> 4;
 }
--- color.h
+++ color.h
@@ -9,6 +9,20 @@ typedef struct
     uint16_t bg, fg;
 } coloring_descr_t;
 
+extern const uint8_t OFFSET_MASK, LENGTH_MASK;
+
+extern const int OFFSET_SIZE;
+
+/**
+ * \brief get offset from cloring descriptor
+ */
+uint8_t offset(coloring_descr_t *descr);
+
+/**
+ * \brief get length from cloring descriptor
+ */
+uint8_t length(coloring_descr_t *descr);
+
 /**
  * \brief apply coloring scheme described by descr to line given
  */
